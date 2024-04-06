1ed885fdac9ca3545fc52dd9100fc51c3b61d74f
brueland <wesbrueland@gmail.com>
2024-04-06 15:31:11 -0500

    Update Profile app to include health records and initialize new apes with empty health records. Update profile.html page to show sample healthrecord data

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Ape, Status
+from .models import Ape, HealthRecord, Status
 
 admin.site.register(Ape)
+admin.site.register(HealthRecord)
 admin.site.register(Status)
